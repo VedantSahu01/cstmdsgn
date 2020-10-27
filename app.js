6299217da9221a378695c681d1d3fa535961c318
Vedant Sahu <vedantsahu007@gmail.com>
2020-10-27 11:09:09 +0530

    add cart

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var mongoose = require('mongoose');
 app.use(express.static('public'))
 mongoose.connect('mongodb://127.0.0.1:27017/ecom', {useNewUrlParser: true})
 var db = mongoose.connection;
+var cartSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  price: Number,
+  quantity: Number,
+});
 var productSchema = new mongoose.Schema({
   name: String,
   image: String,
@@ -16,9 +22,10 @@ var productSchema = new mongoose.Schema({
   desc: String,
   tags: [],
 });
+var Cart = mongoose.model('Cart', cartSchema);
 var Product = mongoose.model('Product', productSchema );
-const initials = [{ name: '1st Product', price: 100 }, { name: '2nd Product', price:200 }];
-// Product.insertMany(initials, function(err, docs) {
+const initials = [{ name: '1st Product', price: 100 ,quantity:1}, { name: '2nd Product', price:200, quantity:1 }];
+// Cart.insertMany(initials, function(err, docs) {
 //   if(err)
 //     console.log('Error on inserting initials');
 // });
@@ -29,8 +36,17 @@ app.get('/', (req, res) => {
     else
       console.log("Error");
   });
-
 });
+
+app.get('/cart', (req, res) => {
+  Cart.find({}, function(err,cart){
+    if(!err)
+      res.render(__dirname + '/cart.ejs', {cart:cart})
+    else
+      console.log("Error");
+  });
+});
+
 
 
 
