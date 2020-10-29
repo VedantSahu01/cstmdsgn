0b339cbf46fdbf6baf628d3f544c791e0d4316b1
Vedant Sahu <vedantsahu007@gmail.com>
2020-10-29 06:32:49 +0530

    add product page

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const port = 3000;
 const express = require('express')
 const app = express();
 var mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 mongoose.connect('mongodb://127.0.0.1:27017/ecom', {useNewUrlParser: true})
 var db = mongoose.connection;
 var cartSchema = new mongoose.Schema({
@@ -24,10 +28,10 @@ var productSchema = new mongoose.Schema({
 });
 var Cart = mongoose.model('Cart', cartSchema);
 var Product = mongoose.model('Product', productSchema );
-const initials = [{ name: '1st Product', price: 100 ,quantity:1}, { name: '2nd Product', price:200, quantity:1 }];
-// Cart.insertMany(initials, function(err, docs) {
+const initials = [{ name: 'DB Shirt', price: 100 ,quantity:1, tags:['shirt','men'], image: 'https://m.media-amazon.com/images/I/81ep28K+XYL._AC_UL480_FMwebp_QL65_.jpg'}, { name: 'B Shirt', price:200, quantity:1, image: 'https://m.media-amazon.com/images/I/81-5lUQGhjL._AC_UL480_FMwebp_QL65_.jpg', tags:['shirt','men']}];
+// Cart.deleteMany({}, function(err, docs) {
 //   if(err)
-//     console.log('Error on inserting initials');
+//     console.log('Error on deleting initials');
 // });
 app.get('/', (req, res) => {
   Product.find({}, function(err,products){
@@ -46,7 +50,29 @@ app.get('/cart', (req, res) => {
       console.log("Error");
   });
 });
+app.post('/cart', (req, res) => {
+  Cart.insertMany([{
+    name: req.body.name,
+    image: req.body.image,
+    price: req.body.price,
+    quantity:req.body.quantity,
+  }], function(err,cart){
+    if(!err)
+      res.redirect('/cart');
+    else
+      console.log("Error");
+  });
 
+});
+
+app.get('/product/:name', function(req, res) {
+  Product.findOne({'_id':req.params.name},function(err,product){
+    if(!err)
+      res.render(__dirname + '/product.ejs', {product:product})
+    else
+      console.log("Error");
+  });
+});
 
 
 
